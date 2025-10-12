1b1ceada3967629d8d0ed83948eb8dc4b12fa34b
jimmy-qu <jimmyqu123@gmail.com>
2025-10-11 21:56:20 -0400

    Art

--- main.py
+++ main.py
@@ -5,22 +5,68 @@ class tree():
     def __init__(self):
         self.state = True  # True means not on fire, False means on fire
 
-def TreeStatus(Forest):
+def TreeStatus_text(Forest):
     for i in range(len(Forest)):
         if Forest[i].state:
             print(f"Tree {i+1} is not on fire")
         else:
             print(f"Tree {i+1} is on fire")
 
+def TreeStatus_art(Forest, location):
+
+    if len(Forest) == 1:
+        if Forest[0].state:
+            print("  🌳👨  ")
+        else:
+            print("  🔥👨  ")
+    else:
+        ## print tree around a circle with a person next to each tree going in a circle
+        ### Format:
+        ###       1👨
+        ###    8     2
+        ###  7         3
+        ###    6     4
+        ###       5
+        n = len(Forest)
+        for i in range(n):
+            if i == location:
+                art = tree_state_art_person(Forest[i])
+            else:
+                art = tree_state_art(Forest[i])
+            if i == 0:
+                print(" " * (n//2) + art)
+            elif i == n//2:
+                print(art + " " * (n//2) + art)
+            elif i < n//2:
+                print(" " * (n//2 - i) + art + " " * (i*2 - 1) + art)
+            else:
+                print(" " * (i - n//2) + art + " " * ((n - i)*2 - 1) + art)
+
+def tree_state_art(tree):
+    if tree.state:
+        return "🌳"
+    else:
+        return "🔥"
+
+def tree_state_art_person(tree):
+    if tree.state:
+        return "🌳👨"
+    else:
+        return "🔥👨"
+
 def win_condition(Forest):
     return all(t.state for t in Forest)
 
-def Forest_Simulator_2P(Forest):
+def Forest_Simulator_2P(Forest, mode="text"):
     while not win_condition(Forest):
         for i in range(len(Forest)):
             print("--------------------------------------------------")
             print(f"You are on Tree {i+1}\n")
-            TreeStatus(Forest)
+
+            if mode == "art":
+                TreeStatus_art(Forest, i)
+            else:
+                TreeStatus_text(Forest)
             if not Forest[i].state:
                 print("Firefighter's turn")
                 print("Do you want to put out the fire? (y/n): ")
@@ -68,12 +114,15 @@ def CPU_Turn_Medium(tree_index, Forest):
         return False
     
     
-def Forest_Simulator_1P(Forest, mode):
+def Forest_Simulator_1P(Forest, mode, display_mode="text"):
     while not win_condition(Forest):
         for i in range(len(Forest)):
             print("--------------------------------------------------")
             print(f"You are on Tree {i+1}\n")
-            TreeStatus(Forest)
+            if display_mode == "a":
+                TreeStatus_art(Forest, i)
+            else:
+                TreeStatus_text(Forest)
             if not Forest[i].state:
                 print("Firefighter's turn")
                 print("Do you want to put out the fire? (y/n): ")
@@ -98,7 +147,7 @@ def Forest_Simulator_1P(Forest, mode):
 if __name__ == "__main__":
 
     game = True
-    while (game = True):
+    while (game == True):
         ## Initial Setup
         print("Firefighter and Pyromaniac Simulation")
         print("How many trees: ")
@@ -120,6 +169,10 @@ if __name__ == "__main__":
                 x = random.randint(0, len(Forest)-1)
             Forest[x].state = False
 
+        ## Display Mode
+        print("Display Mode: Text or Art (t/a): ")
+        display_mode = input().lower()
+
 
         ## Choose Game Mode
         print("Single Player Mode OR Multi Player Mode, choose 1 or 2: ")
@@ -139,9 +192,9 @@ if __name__ == "__main__":
                 difficulty = input().lower()
 
             print("Starting One Player Mode")
-
+            
             if difficulty == 'e':
-                Forest_Simulator_1P(Forest, mode="easy")
+                Forest_Simulator_1P(Forest, mode="easy", )
             else:
                 Forest_Simulator_1P(Forest, mode="medium")
         else:
